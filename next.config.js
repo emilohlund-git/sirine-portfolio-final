e9944ab1bda980e9022329f130400fc9eb797df8
Emilolund <emil@emilohlund.dev>
2023-06-27 20:12:57 +0200

    Added remote server as image host

--- next.config.js
+++ next.config.js
@@ -6,7 +6,13 @@ const nextConfig = {
     return config;
   },
   images: {
-    domains: ["127.0.0.1", "localhost", "https://sirine.fly.dev"],
+    domains: [
+      "127.0.0.1",
+      "localhost",
+      "https://sirine.fly.dev/api",
+      "https://sirine.fly.dev/",
+      "https://sirine.fly.dev",
+    ],
   },
 };
 
