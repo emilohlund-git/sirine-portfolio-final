5a667f74407e6d8c3c4a972868d21f29a0a35165
Emilolund <emil@emilohlund.dev>
2023-06-28 13:23:22 +0200

    📖 Content writing & truncating banner text

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -14,21 +14,21 @@ const Cards = (props: Props) => {
         <div className="flex flex-col self-end gap-y-4 max-w-[80%]">
           <DesignIcon />
           <h4 className="font-medium text-2xl text-transparent bg-clip-text bg-gradient-to-r from-white via-gray-300 to-gray-900">UI/UX</h4>
-          <p className="text-transparent text-md bg-clip-text bg-gradient-to-r from-white via-gray-300 to-gray-400">Designing interfaces that are intuitive, efficient, and enjoyable to use.</p>
+          <p className="text-transparent text-md bg-clip-text bg-gradient-to-r from-white via-gray-300 to-gray-400">Designing user friendly, efficiency, and enjoyable interfaces.</p>
         </div>
       </Card>
       <Card text='Design & Creative'>
         <div className="flex flex-col self-end gap-y-4 max-w-[80%]">
           <BrandingIcon />
           <h4 className="font-medium text-2xl text-transparent bg-clip-text bg-gradient-to-r from-white via-gray-300 to-gray-900">Branding</h4>
-          <p className="text-transparent text-md bg-clip-text bg-gradient-to-r from-white via-gray-300 to-gray-400">Designing interfaces that are intuitive, efficient, and enjoyable to use.</p>
+          <p className="text-transparent text-md bg-clip-text bg-gradient-to-r from-white via-gray-300 to-gray-400">Creating impactful brand identities that inspire trust and resonate with audiences.</p>
         </div>
       </Card>
       <Card text='Product Design'>
         <div className="flex flex-col self-end gap-y-4 max-w-[80%]">
           <ProductDesign />
           <h4 className="font-medium text-2xl text-transparent bg-clip-text bg-gradient-to-r from-white via-gray-300 to-gray-900">Product Design</h4>
-          <p className="text-transparent bg-clip-text bg-gradient-to-r from-white via-gray-300 to-gray-400">Designing interfaces that are intuitive, efficient, and enjoyable to use.</p>
+          <p className="text-transparent bg-clip-text bg-gradient-to-r from-white via-gray-300 to-gray-400">Crafting user-centered products that seamlessly blend form and function.</p>
         </div>
       </Card>
     </div>
--- src/components/ProjectBanner.tsx
+++ src/components/ProjectBanner.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { truncate } from "../utils/general.utils";
 import { getImage } from "../utils/pb.utils";
 
 type Props = {
@@ -18,8 +19,8 @@ const ProjectBanner: React.FC<Props> = ({ project, imageAlignment, href }) => {
       </div>
       <div className={`w-full h-full flex gap-y-4 justify-center px-20 lg:px-40 flex-col border-[1px] bg-gradient-to-tr from-base-300 via-base-100 to-base-200 border-gray-800 order-2 ${imageAlignment === 'left' ? 'lg:order-2' : 'lg:order-1'}`}>
         <h1 className="font-bold text-4xl w-fit text-transparent bg-clip-text bg-gradient-to-r from-white via-gray-300 to-gray-400">{project.title}</h1>
-        <div className="font-normal truncate line-clamp-1 text-md text-transparent bg-clip-text bg-gradient-to-r from-white via-gray-300 to-gray-400"
-          dangerouslySetInnerHTML={{ __html: project.about }} />
+        <div className="font-normal text-md w-full text-transparent bg-clip-text bg-gradient-to-r from-white via-gray-300 to-gray-400"
+          dangerouslySetInnerHTML={{ __html: truncate(project.about, 125) }} />
         <span className="font-semibold pl-2 link link-secondary"><Link href={href}>&gt; Read more</Link></span>
       </div>
     </div>
