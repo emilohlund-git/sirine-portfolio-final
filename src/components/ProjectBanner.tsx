1ed547f69108f9b4095fbc071b5c5dd0dc71cc46
Emilolund <emil@emilohlund.dev>
2023-07-05 21:26:31 +0200

    🎉 Added badge list of roles per project overview

--- src/components/ProjectBanner.tsx
+++ src/components/ProjectBanner.tsx
@@ -20,7 +20,14 @@ const ProjectBanner: React.FC<Props> = ({ project, imageAlignment, href }) => {
       <div className={`w-full h-full flex gap-y-4 justify-center px-20 lg:px-40 flex-col border-[1px] bg-gradient-to-tr from-base-300 via-base-100 to-base-200 border-gray-800 order-2 ${imageAlignment === 'left' ? 'lg:order-2' : 'lg:order-1'}`}>
         <h1 className="font-bold text-4xl w-fit text-transparent bg-clip-text bg-gradient-to-r from-white via-gray-300 to-gray-400">{project.title}</h1>
         <div className="font-normal text-md w-full text-transparent bg-clip-text bg-gradient-to-r from-white via-gray-300 to-gray-400"
-          dangerouslySetInnerHTML={{ __html: truncate(project.about, 125) }} />
+          dangerouslySetInnerHTML={{ __html: truncate(project.about, 130) }} />
+        <div className="flex flex-wrap gap-x-2">
+          {project.roles.map((role, index) => {
+            return (
+              <div key={role + index} className="badge badge-secondary badge-outline">{role}</div>
+            )
+          })}
+        </div>
         <span className="font-semibold pl-2 link link-secondary"><Link href={href}>&gt; Read more</Link></span>
       </div>
     </div>
