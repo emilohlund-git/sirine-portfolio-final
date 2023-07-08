a0e7e32e8fa3fc804ca005db682e4da4ba5b6267
Emilolund <emil@emilohlund.dev>
2023-07-08 17:34:10 +0200

    🧹 Fixed responsitivity

--- src/components/common/ResearchContainer.tsx
+++ src/components/common/ResearchContainer.tsx
@@ -31,11 +31,11 @@ const ResearchContainer: React.FC<Props> = ({ research, title }) => {
 
   return (
     <GridContainer cols={10} className="flex">
-      <GridBox background="white" className="grid col-span-10 h-[20rem] min-h-[20rem]">
+      <GridBox background="white" className="grid col-span-10 h-[20rem] min-h-[20rem] break-words">
         <GridBoxHeader color="gray">{title}</GridBoxHeader>
         {research.map((research) =>
           <div className="flex flex-col break-all max-w-full" key={research.id}>
-            <h4 className="font-light text-2xl mb-2">{research.content}</h4>
+            <h4 className="font-light text-2xl mb-2 break-words">{research.content}</h4>
           </div>
         )}
       </GridBox>
